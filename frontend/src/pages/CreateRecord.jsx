51bbf58603ca27b32741be82c3319d840374477b
MANIKRISHNA <marumallamanikrishna123@gmail.com>
2026-04-16 19:35:53 +0530

    change in delete record

--- frontend/src/pages/CreateRecord.jsx
+++ frontend/src/pages/CreateRecord.jsx
@@ -25,11 +25,11 @@ const CreateRecord = () => {
     e.preventDefault();
 
     try {
-       const payload = {
+       setFormData( {
             ...formData,
             amount: Number(formData.amount),
             viewer_Id: Number(formData.viewer_Id)
-        };
+        });
        const res = await axios.post(
         "https://finance-data-access-api.onrender.com/user_records/create",
         formData,
