8b9c613d9a5834dd6d9933c4d0fc8b81da9636d3
MANIKRISHNA-doctom <marumallamanikrishna123@gmail.com>
2026-04-16 17:29:28 +0530

    Update index.js

--- finance-backend/index.js
+++ finance-backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://finance-data-access-backend.vercel.app",
   credentials: true
 }));
 const port = process.env.PORT || 5000; 
